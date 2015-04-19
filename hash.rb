26722da6938ed5913261b916713163e60fd81d53
Ellis Gray <ellisgray2012@gmail.com>
2015-04-19 21:12:21 +1000

    cleaned up

--- hash.rb
+++
@@ -1,8 +0,0 @@
-class Hash
-  def deep_stringify_keys
-    new_hash = {}
-    self.each do |key, value|
-      new_hash.merge!(key.to_s => (value.is_a?(Hash) ? value.deep_stringify_keys : value))
-    end
-  end
-end
