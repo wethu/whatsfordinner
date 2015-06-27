b5fd2672cab274ba16ede1fa507f8d121016813f
Ellis Gray <ellisgray2012@gmail.com>
2015-06-27 14:56:34 +1000

    added console

--- init.rb
+++ init.rb
@@ -14,6 +14,6 @@ ActiveRecord::Base.establish_connection(configuration['development'])
 Dir['lib/*.rb'].each { |f| require_relative f }
 
 # Models
-autoload(:Persistence, './models/persistence.rb')
 Dir['models/*.rb'].each { |f| require_relative f }
 Dir['views/*.rb'].each { |f| require_relative f }
+
