0cb9c40080bffd01826e803d65708b6990c41eb0
Ellis Gray <ellisgray2012@gmail.com>
2017-04-01 13:09:58 +1000

    switched to a scope

--- models/dinner.rb
+++ models/dinner.rb
@@ -3,12 +3,7 @@ class Dinner < ActiveRecord::Base
 
   serialize :ingredients, Hash
 
-  class << self
-    def week(multiple = 1)
-      collection = all.shuffle.to_a
-      multiple.times.reduce([]) do |memo, _dinner|
-        memo << collection.slice!(0..6)
-      end
-    end
-  end
+  scope :week, ->(amount = 1) {
+    limit(amount * 7).order("RANDOM()").each_slice(7).to_a
+  }
 end
